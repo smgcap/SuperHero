bf32efda96c08c2e23b2214a05967fd266933615
smgcap <79072219+smgcap@users.noreply.github.com>
2022-12-26 23:43:46 +0530

    Add files via upload

---
+++ SuperHeroApi/SuperHeroApi/Controllers/SuperHeroController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SuperHeroApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SuperHeroController : Controller
+    {
+      
+        [HttpGet]
+        public async Task<ActionResult<List<SuperHero>>> GetSuperHeros()
+        {
+            return new List<SuperHero>
+            {
+                new SuperHero
+                {
+                    Name = "Spider Man",
+                    First = "Peter",
+                    Last = "Parker",
+                    Place = "New York City"
+
+                }
+            }; 
+        }
+
+    }
+}
---
+++ SuperHeroApi/SuperHeroApi/SuperHero.cs
@@ -0,0 +1,11 @@
+﻿namespace SuperHeroApi
+{
+    public class SuperHero
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string First { get; set; } = string.Empty;
+        public string Last { get; set; } = string.Empty;
+        public string Place { get; set; } = string.Empty;
+    }
+}
